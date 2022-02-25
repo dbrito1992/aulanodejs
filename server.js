de34d4d62712655d5beab42404d6ff62f5fc4ca4
Diego Brito <1992diego1992@gmail.com>
2022-02-25 17:30:35 -0300

    Mongoose

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const routes = require('./routes');
 const path = require('path');
+const conc = require('mongoose');
+conc.connect(process.env.URLDATANASE).then((res)=>{
+    app.emit("Pronto");
+}).catch(e => console.log(e));
 const {middlewareGlobal} = require('./src/middlewares/middlewares');
-
 // Recebe um corpo de dados na requesição
 app.use(express.urlencoded({extended: true}));
 
@@ -14,7 +18,8 @@ app.set('view engine', 'ejs');
 
 app.use(middlewareGlobal);
 app.use(routes);
-
-app.listen(3000, ()=>{
-    console.log("Servidor Ligado!");
+app.on('Pronto', ()=>{
+    app.listen(3000, ()=>{
+        console.log("Servidor Ligado!");
+    })
 })
--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,3 +1,12 @@
+const HomeModel = require('../models/HomeModel');
+
+HomeModel.create({
+    titulo: "titulo de teste.",
+    descricao: "descrição de teste."
+})
+.then(res=> console.log(res))
+.catch(e=> console.log(e));
+
 exports.index = (req, res)=>{
     res.render('home')
 }
